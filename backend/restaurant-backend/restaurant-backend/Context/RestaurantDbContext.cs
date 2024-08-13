3e16b2711318a49820133ec44b60d88e58f9591a
Berk Polat <berkpolat125@gmail.com>
2024-08-13 12:37:21 +0300

    models and db context added

---
+++ backend/restaurant-backend/restaurant-backend/Context/RestaurantDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restaurant_backend.Models;
+
+namespace restaurant_backend.Context
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext()
+        {
+
+        }
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+    }
+
+   
+}
---
+++ backend/restaurant-backend/restaurant-backend/Models/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace restaurant_backend.Models
+{
+    public class Payment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PaymentID { get; set; }
+
+        [ForeignKey("Order")]
+        public int OrderID { get; set; }
+        public DateTime PaymentTime { get; set; }
+        public string PaymentMethod { get; set; }
+        public decimal AmountPaid { get; set; }
+
+        //public Order Order { get; set; }
+    }
+}
