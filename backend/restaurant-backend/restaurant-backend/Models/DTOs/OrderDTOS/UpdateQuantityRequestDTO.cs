3c7d469fd93c17f5d2d23f5fe509fd321c134a9f
Berk Polat <berkpolat125@gmail.com>
2024-09-11 13:30:18 +0300

    quantity dto added

---
+++ backend/restaurant-backend/restaurant-backend/Models/DTOs/OrderDTOS/UpdateQuantityRequestDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace restaurant_backend.Models.DTOs.OrderDTOS
+{
+    public class UpdateQuantityRequestDTO
+    {
+        public int newQuantity { get; set; }
+    }
+}
--- backend/restaurant-backend/restaurant-backend/Src/Services/OrderItemService.cs
+++ backend/restaurant-backend/restaurant-backend/Src/Services/OrderItemService.cs
@@ -224,5 +224,7 @@ namespace restaurant_backend.Src.Services
         }
 
         
+
+        
     }
 }
