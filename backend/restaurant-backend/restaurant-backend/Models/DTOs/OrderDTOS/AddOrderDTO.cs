06bf47dce9f05c47d5331cf457efa6460f181375
Berk Polat <berkpolat125@gmail.com>
2024-09-24 13:23:08 +0300

    Order table, dto , service updated

--- backend/restaurant-backend/restaurant-backend/Models/DTOs/OrderDTOS/AddOrderDTO.cs
+++ backend/restaurant-backend/restaurant-backend/Models/DTOs/OrderDTOS/AddOrderDTO.cs
@@ -6,6 +6,11 @@
         public int TableNumber { get; set; }
         public DateTime OrderTime { get; set; }
         public double TotalPrice { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int Quantity { get; set; }
+
         public string OrderStatus { get; set; }
     }
 }
